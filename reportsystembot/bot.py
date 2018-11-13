db9b0a3d14a7f45fc5944425f8e946f8451c401f
Afonso Rodrigues <afonsoaugustoventura@gmail.com>
2018-11-13 18:33:02 +0000

    Modificação de responsabilidades nas classes e rename dos arquivos.

--- reportsystembot/bot.py
+++ reportsystembot/bot.py
@@ -7,7 +7,7 @@ rso = ReportSO()
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.DEBUG)
+                     level=logging.INFO)
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
