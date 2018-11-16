9a2de6b3a9c3de32678e1ba75e3874ef93899a3a
Afonso Rodrigues <afonsoaugustoventura@gmail.com>
2018-11-16 20:59:27 -0200

    Correcao da  issue #1.

--- reportsystembot/audit.py
+++ reportsystembot/audit.py
@@ -2,23 +2,16 @@ import functools
 import logging
 import time
 
-
-#parametros de configuração do logger
 def timed(logger, level=None, format='%s: %s ms'):
     if level is None:
         level = logging.DEBUG
 
     def decorator(fn):
         @functools.wraps(fn)
-        def inner(self_bot,bot,update,*args, **kwargs):
+        def inner(*args, **kwargs):
             start = time.time()
             result = fn(self_bot,bot,update,*args, **kwargs)
             duration = time.time() - start
-            print('Args ------- ', *args)
-            print('self_bot ------- ', type(self_bot))
-            print('Bot ------- ', type(bot))
-            print('Updater -   ---- -- ',type(update))
-            #print('type', [ type(x) for x in args[2]])
             logger.log(level, format, repr(fn), duration * 1000)
             return result
         return inner
